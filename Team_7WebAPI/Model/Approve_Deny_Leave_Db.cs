43137beee43b272944b964fe7f93bd2637be15d5
Kaushikk123 <kulkarnikaushik2121@gmail.com>
2022-07-12 19:17:41 +0530

    Approve_Deny_Leave Added

---
+++ Team_7WebAPI/Model/Approve_Deny_Leave_Db.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team_7WebAPI.Model
+{
+    public class Approve_Deny_Leave_Db
+    {
+        public int Employee_Id { get; set; }
+        public String Employee_Name { get; set; }
+        public int Leave_Balance { get; set; }
+        public DateTime Start_Date { get; set; }
+        public DateTime End_Date { get; set; }
+        public int Number_Of_Days { get; set; }
+        public string Leave_types { get; set; }
+        public string Reason { get; set; }
+    }
+}
