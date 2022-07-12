45f931de25d33c9a6de6585a096564d4f352bbb2
ariz1710 <arizahmad9861@gmail.com>
2022-07-12 17:53:22 +0530

    Added Manager Details

---
+++ Team_7WebAPI/Model/Manager_Details_Section_View.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team_7WebAPI.Model
+{
+    public class Manager_Details_Section_VIew
+    {
+        public int Employee_ID { get; set; }
+        public string Full_Name { get; set; }
+        public string Email_Address { get; set; }
+        public int Mobile_Number { get; set; }
+    }
+}
