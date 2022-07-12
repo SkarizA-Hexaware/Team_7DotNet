033f115ef6ef5e789a1af214ea8d682e0c817d3a
DhanashreeC022 <dhanashreechore@gmail.com>
2022-07-12 18:09:11 +0530

    My Leave Application

---
+++ Team_7WebAPI/Model/My_Leave_Application_Section_Db.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team_7WebAPI.Model
+{
+    public class My_Leave_Application_Section_Db
+    {
+        public int Leave_ID { get; set; }
+        public int Number_of_Days { get; set; }
+        public DateTime Start_Date { get; set; }
+        public DateTime End_Date { get; set; }
+        public string Leave_Type { get; set; }
+        public string Status { get; set; }
+        public string Reason { get; set; }
+        public DateTime Applied_ON { get; set; }
+        public string Manager_comments { get; set; }
+    }
+
+    
+}
