eea906df82289a68dc6db546b1553a428a9ad517
Nandkishor8777 <patilnandkishor06@gmail.com>
2022-07-12 17:27:59 +0530

    My_Details_Section

---
+++ Team_7WebAPI/Model/My_Details_View.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team_7WebAPI.Model
+{
+    public class My_Details_View
+    {
+        public int Employee_Id { get; set; }
+        public string Full_Name { get; set; }
+
+        public string Email_Address { get; set; }
+
+        public long Mobile_Number { get; set; }
+
+        public DateTime Date_Joined { get; set; }
+        public string Department { get; set; }
+
+        public int Available_Leave_Balance { get; set; }
+
+    }
+}
